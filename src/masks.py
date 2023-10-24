d2965a722e09666dfd02b22eff8d5be651aedd67
Mary <kass.o@yandex.ru>
2023-10-24 09:29:29 +0300

    add type replacement masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,7 @@
+from typing import Any
 
 
-def card_number_encoder(number_card: str) -> str:
+def card_number_encoder(number_card: str) -> Any:
     """Функция принимает номер карты и зашифровывает его"""
     if len(number_card) == 16:
         return f"{number_card[0:4]} {number_card[5:7]}** **** {number_card[12:17]}"
@@ -8,7 +9,7 @@ def card_number_encoder(number_card: str) -> str:
         return "Номер карты введен неверно"
 
 
-def account_number_encoder(number_encoder: str) -> str:
+def account_number_encoder(number_encoder: str) -> Any:
     """Функция принимает счет карты и зашифровывает его"""
     if len(number_encoder) == 20:
         return f"**{number_encoder[16:21]}"
