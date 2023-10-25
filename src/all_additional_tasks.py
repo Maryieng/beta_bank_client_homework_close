141e543b7d6093b9d29b27066cf90a61c5b36aa8
Mary <kass.o@yandex.ru>
2023-10-25 19:06:36 +0300

    add func from dictionary.py in all_additional_tasks.py. and rename

--- src/all_additional_tasks.py
+++ src/all_additional_tasks.py
@@ -1,5 +1,21 @@
 from operator import mul
 from typing import Any
+import os
+import os.path
+
+
+def directory_dictionary(path="../src", recursive_counting=None) -> None:
+    """Функция принимает путь до директории и выводит словарь с содержимым"""
+    print(
+        f"""files: {len([name for name in os.listdir(path) if os.path.isfile(name)])}
+folders: {len([name for name in os.listdir(path) if not os.path.isfile(name)])}"""
+    )
+    if recursive_counting is not None:
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                print(os.path.join(root, name))
+            for name in dirs:
+                print(os.path.join(root, name))
 
 
 def first_and_last_letter(list_strings: list[str]) -> list[str]:
