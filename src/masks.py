ed1f7a9f8722e3c124dd62a3fef0b33dbf2e73f3
Mary <kass.o@yandex.ru>
2023-10-31 18:47:58 +0300

    replacing slices

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ from typing import Any
 def card_number_encoder(number_card: str) -> Any:
     """Функция принимает номер карты и зашифровывает его"""
     if len(number_card) == 16:
-        return f"{number_card[0:4]} {number_card[5:7]}** **** {number_card[12:17]}"
+        return f"{number_card[0:4]} {number_card[4:6]}** **** {number_card[12:17]}"
     if len(number_card) != 16:
         return "Номер карты введен неверно"
 
