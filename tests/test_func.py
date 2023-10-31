f6c685564e262a8f6db1950ba4523a5984ff37d2
Mary <kass.o@yandex.ru>
2023-10-31 20:52:56 +0300

    tests done for masks and widget

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,5 +1,5 @@
-from src.masks import card_number_encoder
-from src.masks import account_number_encoder
+from src.masks import account_number_encoder, card_number_encoder
+from src.widget import mask_with_card_type, convert_to_date
 import pytest
 #from src.all_additional_tasks import directory_dictionary
 #from src.all_additional_tasks import first_and_last_letter
@@ -7,8 +7,6 @@ import pytest
 #from src.all_additional_tasks import product_sorting
 #from src.processing import list_dictionaries_with_key
 #from src.processing import sorted_list_of_dict
-#from src.widget import mask_with_card_type
-#from src.widget import convert_to_date
 
 
 @pytest.fixture
@@ -31,3 +29,14 @@ def test_account_number_encoder(my_account_card):
     assert account_number_encoder(my_account_card) == '**4305'
     assert account_number_encoder('73654108430135874305111') == "Номер счета введен неверно"
     assert account_number_encoder('') == "Номер счета введен неверно"
+
+
+def test_mask_with_card_type():
+    assert mask_with_card_type('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 6361'
+    assert mask_with_card_type('счет 73654108430135874305') == 'Счет **4305'
+    assert mask_with_card_type('счет 7365410843013587430511') == "Счет Номер счета введен неверно"
+    assert mask_with_card_type('Visa Platinum 700079228960636111') == "Visa Platinum Номер карты введен неверно"
+
+
+def test_convert_to_date():
+    assert convert_to_date("2018-07-11T02:26:18.671407") == "11.07.2018"
