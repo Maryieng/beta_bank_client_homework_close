605755a239c260ff3f4356cfdbcfd00f590c301c
Mary <kass.o@yandex.ru>
2023-11-01 12:15:58 +0300

    final editing

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,6 +1,7 @@
-from src.masks import account_number_encoder, card_number_encoder
-from src.widget import mask_with_card_type, convert_to_date
 import pytest
+
+from src.masks import account_number_encoder, card_number_encoder
+from src.widget import convert_to_date, mask_with_card_type
 
 
 @pytest.fixture
@@ -25,15 +26,12 @@ def test_account_number_encoder(my_account_card):
     assert account_number_encoder('') == "Номер счета введен неверно"
 
 
-def test_mask_with_card_type():
-    assert mask_with_card_type('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 6361'
-    assert mask_with_card_type('счет 73654108430135874305') == 'Счет **4305'
-    assert mask_with_card_type('счет 7365410843013587430511') == "Счет Номер счета введен неверно"
-    assert mask_with_card_type('Visa Platinum 700079228960636111') == "Visa Platinum Номер карты введен неверно"
+@pytest.mark.parametrize("input_value, expected_result",
+                         [('счет 73654108430135874305', 'Счет **4305'),
+                          ('Visa Platinum 7000792289606361', "Visa Platinum 7000 79** **** 6361")])
+def test_mask_with_card_type(input_value, expected_result):
+    assert mask_with_card_type(input_value) == expected_result
 
 
 def test_convert_to_date():
     assert convert_to_date("2018-07-11T02:26:18.671407") == "11.07.2018"
-
-
-
