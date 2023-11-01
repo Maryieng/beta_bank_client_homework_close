0ed7208cb5705ba1e07d7fb60f71ee601530932b
Mary <kass.o@yandex.ru>
2023-11-01 19:01:47 +0300

    new tests in processing

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.masks import account_number_encoder, card_number_encoder
+from src.processing import list_dictionaries_with_key, sorted_list_of_dict
 from src.widget import convert_to_date, mask_with_card_type
 
 
@@ -35,3 +36,28 @@ def test_mask_with_card_type(input_value, expected_result):
 
 def test_convert_to_date():
     assert convert_to_date("2018-07-11T02:26:18.671407") == "11.07.2018"
+
+
+@pytest.mark.parametrize('lst, exit',
+                         [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+                           [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])])
+def test_list_dictionaries_with_key(lst, exit):
+    assert list_dictionaries_with_key(lst) == exit
+
+
+@pytest.mark.parametrize("input_value, expected_result",
+                         [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+                           [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])])
+def test_sorted_list_of_dict(input_value, expected_result):
+    assert sorted_list_of_dict(input_value, False) == expected_result
+    assert sorted_list_of_dict(input_value, True) == expected_result[::-1]
