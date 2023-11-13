8c744f4c6e6d02b374115f4ac168bcdfb07b0bd5
Mary <kass.o@yandex.ru>
2023-11-13 17:45:52 +0300

    add

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,16 +5,14 @@ from src.decorators import log
 
 def test_log_to_file() -> None:
     filename = "test_logs.txt"
+
     @log(filename=filename)
     def my_function(param1: int, param2: int) -> int:
         return param1 + param2
-
     my_function(10, 2)
-
     with open(filename, 'r') as file:
         logs = file.read()
     now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-
     expected_logs = f"{now}, my_function, ok\n"
     assert logs == expected_logs
 
