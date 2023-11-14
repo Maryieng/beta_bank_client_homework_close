06fd6acc7e324ebb566b5fd6c15972460f39a42f
Mary <kass.o@yandex.ru>
2023-11-14 20:57:41 +0300

    editing functions

--- src/utils.py
+++ src/utils.py
@@ -3,31 +3,34 @@ import os
 from typing import Any
 
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def getting_data_from_file(filename: str) -> Any:
-    """ считывает из файла данные """
+    """ принимает на вход путь до файла и возвращает список словарей. Если есть ошибка,
+    функция возвращает пустой список """
     current_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.join(current_directory, 'data', filename)
-    with open(file_path, encoding='utf-8') as file:
-        try:
+    try:
+        with open(file_path, encoding='utf-8') as file:
             return json.load(file)
-        except json.JSONDecodeError:
-            return []
+    except json.JSONDecodeError:
+        return []
+    except FileNotFoundError:
+        return []
 
 
-list_of_operations = getting_data_from_file('operations.json')
-
-
-def transaction_amount_in_rubles(id_operation: int) -> Any:
+def transaction_amount_in_rubles(operation: dict[str, Any]) -> Any:
     """ принимает на вход транзакцию и возвращает сумму транзакции в рублях, возвращает тип float """
-    specific_operation = [operation for operation in list_of_operations
-                          if operation.get("id") == id_operation]
-    operation_dict = specific_operation[0]
-    if operation_dict["operationAmount"]["currency"]["code"] == "RUB":
-        return float(operation_dict["operationAmount"]["amount"])
+    if operation["operationAmount"]["currency"]["code"] == "RUB":
+        return float(operation["operationAmount"]["amount"])
     else:
-        print("ValueError: Операция совершена не в рублях")
-        data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-        dollar_exchange_rate = data['Valute']['USD']['Value']
-        return round(dollar_exchange_rate * float(operation_dict["operationAmount"]["amount"]), 2)
+        api_key = os.getenv('api_key')
+        url = f"http://api.currencylayer.com/live?access_key={api_key}&currencies=RUB&source=USD"
+        response = requests.get(url)
+        data = response.json()
+        conversion_rate = data['quotes']['USDRUB']
+        rub_amount = float(operation["operationAmount"]["amount"]) * conversion_rate
+        return round(rub_amount, 2)
