2de0cc0488c3803d33f97fea704e37e0c3abf628
Mary <kass.o@yandex.ru>
2023-11-16 16:04:37 +0300

    moving loggers to modules with functions

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,29 @@
+import logging
 from typing import Any
+
+logger = logging.getLogger('__masks__')
+file_handler_masks = logging.FileHandler('masks_loger.log', 'w', encoding='utf-8')
+file_formatter_masks = logging.Formatter('%(asctime)s %(module)s %(levelname)s %(message)s')
+file_handler_masks.setFormatter(file_formatter_masks)
+logger.addHandler(file_handler_masks)
+logger.setLevel(logging.INFO)
 
 
 def card_number_encoder(number_card: str) -> Any:
     """ Функция принимает номер карты и зашифровывает его """
     if len(number_card) == 16:
+        logger.info('Номер карты введен верно')
         return f"{number_card[0:4]} {number_card[4:6]}** **** {number_card[12:17]}"
     if len(number_card) != 16:
+        logger.error('Номер карты введен не верно')
         return "Номер карты введен неверно"
 
 
 def account_number_encoder(number_encoder: str) -> Any:
     """ Функция принимает счет карты и зашифровывает его """
     if len(number_encoder) == 20:
+        logger.info('Номер счета введен верно')
         return f"**{number_encoder[16:21]}"
     if len(number_encoder) != 20:
+        logger.error('Номер счета введен не верно')
         return "Номер счета введен неверно"
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from typing import Any
 
@@ -6,6 +7,13 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logger = logging.getLogger('__utils__')
+file_handler_utils = logging.FileHandler('utils_loger.log', 'w', encoding='utf-8')
+file_formatter_utils = logging.Formatter('%(asctime)s %(module)s %(levelname)s %(message)s')
+file_handler_utils.setFormatter(file_formatter_utils)
+logger.addHandler(file_handler_utils)
+logger.setLevel(logging.INFO)
 
 
 def getting_data_from_file(filename: str) -> Any:
@@ -15,16 +23,20 @@ def getting_data_from_file(filename: str) -> Any:
     file_path = os.path.join(current_directory, 'data', filename)
     try:
         with open(file_path, encoding='utf-8') as file:
+            logger.info(f'файл {filename} загружен')
             return json.load(file)
     except json.JSONDecodeError:
+        logger.error(f'файл {filename} не загружен')
         return []
     except FileNotFoundError:
+        logger.error(f'файл {filename} не найден')
         return []
 
 
 def transaction_amount_in_rubles(operation: dict[str, Any]) -> Any:
     """ принимает на вход транзакцию и возвращает сумму транзакции в рублях, возвращает тип float """
     if operation["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info(f'транзакция в рублях возвращает {operation["operationAmount"]["amount"]} рублей')
         return float(operation["operationAmount"]["amount"])
     else:
         api_key = os.getenv('api_key')
@@ -33,4 +45,5 @@ def transaction_amount_in_rubles(operation: dict[str, Any]) -> Any:
         data = response.json()
         conversion_rate = data['quotes']['USDRUB']
         rub_amount = float(operation["operationAmount"]["amount"]) * conversion_rate
+        logger.info(f'транзакция в долларах возвращает {round(rub_amount, 2)} рублей')
         return round(rub_amount, 2)
