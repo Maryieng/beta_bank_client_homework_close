999140d6b9d986d7ed9ebb1244d1644e7d007047
Mary <kass.o@yandex.ru>
2023-11-22 12:42:43 +0300

    2 functions for searching by string and counting values by category

--- src/reading_data.py
+++ src/reading_data.py
@@ -1,5 +1,8 @@
 import os
-from typing import Any
+import re
+from collections import Counter
+from typing import Any, Counter
+
 import pandas as pd
 
 
@@ -15,3 +18,27 @@ def reading_data_from_file_xlsx(file_name: str) -> Any:
     current_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.join(current_directory, 'data', file_name)
     return pd.read_excel(file_path, na_values=["NA", "N/A", "missing"])
+
+
+def searching_data_by_string(list_dictionaries: list[dict], string: str) -> list[dict]:
+    """ принимает список словарей с данными о банковских операциях и
+    строку поиска и возвращать список словарей, у которых в описании есть данная строка. """
+    result = []
+    pattern = re.compile(string, re.IGNORECASE)
+    for dictionary in list_dictionaries:
+        description = dictionary.get('description', '')
+        if re.search(pattern, description):
+            result.append(dictionary)
+    return result
+
+
+def search_dictionaries_by_category(list_dictionaries: list[dict], category_dictionary: list) -> dict:
+    """ принимает список словарей с данными о банковских операциях и категорий операций. возвращать словарь,
+     в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
+    category_counts: Counter[str] = Counter()
+    for transaction in list_dictionaries:
+        category = transaction.get('description', 'Uncategorized')
+        category_counts[category] += 1
+    for category in category_dictionary:
+        category_counts.setdefault(category, 0)
+    return dict(category_counts)
